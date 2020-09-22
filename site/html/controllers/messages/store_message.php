183583156f7a8d99004656757bb925e474b265ce
Fabio Marques <fabiomanuelsilvamarques@gmail.com>
2020-09-22 15:43:29 +0200

    création de message

--- site/html/controllers/messages/store_message.php
+++ site/html/controllers/messages/store_message.php
@@ -1,1 +1,33 @@
 <?php
+session_start();
+include "../../../databases/db_connection.php";
+$_SESSION['errors'] = [];
+$_SESSION['old_post'] = $_POST;
+$auth_user = 'fabio10000';
+if (empty($_POST['sendTo']))
+    $_SESSION['errors'][] = 'sendTo';
+else {
+    $query = $file_db->prepare("SELECT COUNT(*) FROM users WHERE username = ?");
+    $query->execute([$_POST['sendTo']]);
+    // if user not found
+    if ($query->fetchColumn() != 1) $_SESSION['errors'][] = 'sendTo';
+}
+if (empty($_POST['subject'])) $_SESSION['errors'][] = 'subject';
+if (empty($_POST['content'])) $_SESSION['errors'][] = 'content';
+
+if (sizeof($_SESSION['errors']) > 0) {
+    header('Location: /views/messages/new_message.php');
+    exit;
+} else {
+    $_SESSION['errors'] = null;
+    $query = $file_db->prepare("INSERT INTO messages (subject, content, `time`, `from`, `to`) VALUES (?, ?, ?, ?, ?)");
+    $query->execute([
+        $_POST['subject'],
+        $_POST['content'],
+        time(),
+        $auth_user,
+        $_POST['sendTo']
+    ]);
+    header("Location: /views/messages/messages.php");
+    exit;
+}
